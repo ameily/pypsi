701f6ac5c910e061ed1623b779be1f5bc6e23672
Adam Meily <meily.adam@gmail.com>
2013-12-12 09:09:47 -0500

    added pipe attribute to Command

--- pypsi/base.py
+++ pypsi/base.py
@@ -10,10 +10,11 @@ class InputPreprocessor(object):
 
 class Command(object):
 
-    def __init__(self, name, usage=None, category=None):
+    def __init__(self, name, usage=None, category=None, pipe='str'):
         self.name = name
         self.usage = usage
         self.category = category
+        self.pipe = pipe
 
     def run(self, shell, ctx):
         raise NotImplementedError()
