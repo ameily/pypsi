e211b5952191742c86b3771d465971750855b94e
Adam Meily <meily.adam@gmail.com>
2013-12-16 22:08:27 -0500

    added Plugin.setup() base function

--- pypsi/base.py
+++ pypsi/base.py
@@ -1,18 +1,28 @@
 
 
 class Plugin(object):
-    pass
+
+    def setup(self, shell):
+        return 0
 
 
 class Preprocessor(object):
-    pass
+
+    def on_input(self, shell, line):
+        return line
+
+    def on_tokenize(self, shell, tokens):
+        return tokens
+
+    def on_input_canceled(self, shell):
+        return 0
 
 
 class Command(object):
 
     def __init__(self, name, usage=None, category=None, pipe='str'):
         self.name = name
-        self.usage = usage
+        self.usage = usage or ''
         self.category = category
         self.pipe = pipe
 
