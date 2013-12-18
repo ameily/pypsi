8a9f5249c3aa546269135b5ef3a03e5ec48a08a4
Adam Meily <meily.adam@gmail.com>
2013-12-17 23:58:38 -0500

    added arguments

--- pypsi/commands/xargs.py
+++ pypsi/commands/xargs.py
@@ -1,26 +1,48 @@
 
-from pypsi.base import Command
+from pypsi.base import Command, PypsiArgParser
 import sys
+import argparse
 
-XArgsUsage = """
-"""
+XArgsUsage = "%(prog)s [-h] [-I repstr] command"
 
 class XArgsCommand(Command):
 
     def __init__(self, name='xargs', **kwargs):
-        super(XArgsCommand, self).__init__(name=name, **kwargs)
+        self.parser = PypsiArgParser(
+            prog=name,
+            description='build and execute command lines from stdin',
+            usage=XArgsUsage
+        )
+
+        self.parser.add_argument(
+            '-I', default='{}', action='store',
+            metavar='repstr', help='string token to replace',
+            dest='token'
+        )
+
+        self.parser.add_argument(
+            'command', nargs=argparse.REMAINDER, help="command to execute"
+        )
+
+        super(XArgsCommand, self).__init__(name=name, usage=self.parser.format_help(), **kwargs)
 
     def run(self, shell, args, ctx):
-        argc = len(args)
-        if not argc:
+        ns = self.parser.parse_args(args)
+        if self.parser.last_error:
+            self.error(shell, self.parser.last_error)
+            return 1
+
+        if not ns.command:
+            self.error(shell, "missing command")
             return 1
 
         base = ' '.join([
-            '"{}"'.format(c.replace('"', '\\"')) for c in args
+            '"{}"'.format(c.replace('"', '\\"')) for c in ns.command
         ])
 
         child = ctx.fork()
         for line in sys.stdin:
-            cmd = base.replace('{}', line.strip())
+            cmd = base.replace(ns.token, line.strip())
             shell.execute(cmd, child)
+
         return 0
