96853736c3df05a357e5467cdd912f9f80f976be
Adam Meily <meily.adam@gmail.com>
2013-12-20 00:25:47 -0500

    * added fallback command support * added catching of OSError if command doesn't exist

--- pypsi/commands/system.py
+++ pypsi/commands/system.py
@@ -2,7 +2,6 @@
 from pypsi.base import Command
 import subprocess
 
-
 class SystemCommand(Command):
 
     def __init__(self, name='system', **kwargs):
@@ -11,14 +10,23 @@ class SystemCommand(Command):
     def run(self, shell, args, ctx):
         rc = None
         proc = None
-        if shell.real_stdin == ctx.stdin:
-            proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        else:
-            proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            proc.stdin.write(ctx.stdin.read())
-            proc.stdin.close()
+        try:
+            if shell.real_stdin == ctx.stdin:
+                proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=ctx.stderr)
+            else:
+                proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=ctx.stderr)
+                proc.stdin.write(ctx.stdin.read())
+                proc.stdin.close()
+        except OSError as e:
+            if e.errno == 2:
+                shell.error(self.name, ": executable not found\n")
+                return -1
 
         for line in iter(proc.stdout.readline, ''):
             ctx.stdout.write(line)
         rc = proc.wait()
-        return rc
+        return rc if rc <= 0 else -1
+
+    def fallback(self, shell, name, args, ctx):
+        args.insert(0, name)
+        return self
