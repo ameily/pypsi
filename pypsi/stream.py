db29aacff8f64e9810fa1615512b5aa06548067d
Adam Meily <meily.adam@gmail.com>
2014-02-04 17:32:42 -0500

    initial commit of std stream proxies

--- pypsi/stream.py
+++ pypsi/stream.py
@@ -28,6 +28,9 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+import sys
+
+
 '''
 Stream classes for writing to files.
 '''
@@ -82,3 +85,122 @@ class PypsiStream(object):
             stream.write(self.postfix)
 
         stream.flush()
+
+
+
+class AnsiColorCodes(object):
+
+    def __init__(self):
+        self.reset = '\x1b[0m'
+        self.gray = '\x1b[1;30m'
+        self.red = '\x1b[1;31m'
+        self.green = '\x1b[1;32m'
+        self.yellow = '\x1b[1;33m'
+        self.blue = '\x1b[1;34m'
+        self.purple = '\x1b[1;35m'
+        self.cyan = '\x1b[1;36m'
+        self.white = '\x1b[1;37m'
+        self.black = '\x1b[0;30m'
+
+        self.codes = {
+            'reset': self.reset,
+            'gray': self.gray,
+            'red': self.red,
+            'green': self.green,
+            'yellow': self.yellow,
+            'blue': self.blue,
+            'purple': self.purple,
+            'cyan': self.cyan,
+            'white': self.white,
+            'black': self.black
+        }
+
+    def __getitem__(self, key):
+        return self.codes[key]
+
+
+class StdStreamProxy(object):
+
+    def __init__(self, fp, colors=AnsiColorCodes()):
+        self._colors = colors
+        self._stack = fp if isinstance(fp, list) else [fp]
+
+    @property
+    def _fp(self):
+        return self._stack[-1]
+
+    def redirect(self, fp):
+        self._stack.append(fp)
+
+    def close(self, target=None):
+        if len(self._stack) > 1:
+            if target is None or target != self._fp:
+                self._fp.close()
+                self._stack.pop()
+
+    def reset(self, fp):
+        self._stack = fp if isinstance(fp, list) else [fp]
+        while self._fp != fp and len(self._stack) > 1:
+            self.close()
+        self.close()
+
+    def color_gray(self, *args):
+        return self.colorize('gray', *args)
+
+    def color_red(self, *args):
+        return self.colorize('red', *args)
+
+    def color_green(self, *args):
+        return self.colorize('green', *args)
+
+    def color_yellow(self, *args):
+        return self.colorize('yellow', *args)
+
+    def color_blue(self, *args):
+        return self.colorize('blue', *args)
+
+    def color_purple(self, *args):
+        return self.colorize('purple', *args)
+
+    def color_cyan(self, *args):
+        return self.colorize('cyan', *args)
+
+    def color_white(self, *args):
+        return self.colorize('white', *args)
+
+    def color_black(self, *args):
+        return self.colorize('black', *args)
+
+    def color_reset(self):
+        return self.colorize('reset')
+
+    def colorize(self, color, *args):
+        #a = args
+        #if self._fp.isatty():
+        #    a.insert(0, self._colors[color])
+        #    a.append(self._colors['reset'])
+        #return a
+        if self._fp.isatty():
+            return self._colors[color]
+        return ''
+
+    def __getattr__(self, attr):
+        return getattr(self._fp, attr)
+
+
+class StdoutProxy(StdStreamProxy):
+
+    def __init__(self, **kwargs):
+        super(StdoutProxy, self).__init__(fp=sys.stdout, **kwargs)
+
+
+class StderrProxy(StdStreamProxy):
+
+    def __init__(self, **kwargs):
+        super(StderrProxy, self).__init__(fp=sys.stderr, **kwargs)
+
+
+class StdinProxy(StdStreamProxy):
+
+    def __init__(self, **kwargs):
+        super(StdinProxy, self).__init__(fp=sys.stdin, **kwargs)
