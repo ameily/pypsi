3fdab244a260274ac6bfd166392d7dbde95cc298
Adam Meily <meily.adam@gmail.com>
2014-03-04 15:49:31 -0500

    removed exit_rc in favor of shell.running

--- pypsi/commands/exit.py
+++ pypsi/commands/exit.py
@@ -43,4 +43,5 @@ Exit the {shell} shell"""
         super(ExitCommand, self).__init__(name=name, usage=self.Usage, topic=topic, brief="exit the shell", **kwargs)
 
     def run(self, shell, args, ctx):
-        return shell.exit_rc
+        shell.running = False
+        return 0
