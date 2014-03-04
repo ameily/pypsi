79187fc8472841a77245f085e88193c31d0835dc
Adam Meily <meily.adam@gmail.com>
2014-03-04 15:53:26 -0500

    add support for history keys

--- pypsi/gui/widgets/stdin.py
+++ pypsi/gui/widgets/stdin.py
@@ -9,6 +9,9 @@ class StdinWidget(QtGui.QLineEdit):
 
     def __init__(self, *args , **kwargs):
         super(StdinWidget, self).__init__(*args, **kwargs)
+        self.history = []
+        self.hist_pos = -1
+        self.tmp_hist = ''
 
     def keyPressEvent(self, event):
         parent = True
@@ -38,9 +41,57 @@ class StdinWidget(QtGui.QLineEdit):
                 elif event.key() == QtCore.Qt.Key_PageDown:
                     self.scrollPage.emit(-1)
                     parent = False
+        elif not event.modifiers():
+            if event.key() == QtCore.Qt.Key_Up:
+                self.move_history(-1)
+            elif event.key() == QtCore.Qt.Key_Down:
+                self.move_history(1)
 
         if parent:
             super(StdinWidget, self).keyPressEvent(event)
+
+    def submit(self, sig):
+        if not sig:
+            item = self.text()
+            if not self.history or item != self.history[-1]:
+                self.history.append(item)
+        self.clear()
+        self.hist_pos = None
+
+    def move_history(self, direction):
+        print("move_history(", direction, ") ==", self.hist_pos)
+        if not self.history:
+            print(">> hist is null")
+            return
+
+        if self.hist_pos is None:
+            if direction > 0:
+                #self.hist_pos = 0
+                self.setText(self.tmp_hist)
+                return
+            else:
+                self.hist_pos = len(self.history) - 1
+        else:
+            self.hist_pos += direction
+
+        if self.hist_pos < 0:
+            #print("hist_pos == -1")
+            #self.clear()
+            self.hist_pos = 0
+            #return
+
+        if self.hist_pos >= len(self.history):
+            print("hist_pos >= len()")
+            #self.hist_pos = len(self.history) - 1
+            self.hist_pos = None
+            self.setText(self.tmp_hist)
+            self.clear()
+            return
+
+        print("hist_pos ==", self.hist_pos)
+        #TODO tmp_hist
+        self.setText(self.history[self.hist_pos])
+        self.setCursorPosition(len(self.text()))
 
     # tab = complete
     # up = history
