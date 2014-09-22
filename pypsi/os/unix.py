4437542ddda70bee43b0fa745ca8dbd2f209a863
Adam Meily <meily.adam@gmail.com>
2014-09-22 12:32:15 -0400

    added os specific path completers

---
+++ pypsi/os/unix.py
@@ -0,0 +1,70 @@
+#
+# Copyright (c) 2014, Adam Meily
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice, this
+#   list of conditions and the following disclaimer in the documentation and/or
+#   other materials provided with the distribution.
+#
+# * Neither the name of the {organization} nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+'''
+Unix (Cygwin, Linux, etc) specific functions
+'''
+
+def unix_path_completer(shell, args, prefix):
+    root = None
+    if args:
+        root = args[-1]
+        if root:
+            if not root.startswith(os.path.sep) and not root.startswith('.' + os.path.sep):
+                root = '.' + os.path.sep + root
+        else:
+            root = '.' + os.path.sep
+    else:
+        root = '.' + os.path.sep
+
+    if root.endswith(prefix) and prefix:
+        root = root[:0 - len(prefix)]
+
+    #return ['prefix:'+prefix, 'root:'+root]
+
+    if not os.path.exists(root):
+        return []
+
+    if os.path.isdir(root):
+        files = []
+        dirs = []
+        for entry in os.listdir(root):
+            full = os.path.join(root, entry)
+            if entry.startswith(prefix):
+                if os.path.isdir(full):
+                    dirs.append(entry + os.path.sep)
+                else:
+                    files.append(entry)
+        files = sorted(files)
+        dirs = sorted(dirs)
+        return dirs + files
+    else:
+        return []
+
