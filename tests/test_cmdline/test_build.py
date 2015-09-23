0aef2ed5e3cfc0b167aa27191283dddad044f2bc
Adam Meily <meily.adam@gmail.com>
2015-09-23 17:11:05 -0400

    add simple stdout redirection test case

--- tests/test_cmdline/test_build.py
+++ tests/test_cmdline/test_build.py
@@ -13,12 +13,30 @@ class TestCmdlineBuild(object):
             self.parser.build([
                 StringToken(0, "echo"),
                 WhitespaceToken(1),
-                StringToken(1, "hello")
+                StringToken(2, "hello")
             ]),
             Statement([
                 CommandInvocation(
                     name="echo",
                     args=["hello"]
+                )
+            ])
+        )
+
+    def test_redirect_stdout(self):
+        assert_equal(
+            self.parser.build([
+                StringToken(0, "echo"),
+                WhitespaceToken(1),
+                StringToken(2, "hello"),
+                OperatorToken(3, ">"),
+                StringToken(4, "output.txt")
+            ]),
+            Statement([
+                CommandInvocation(
+                    name="echo",
+                    args=["hello"],
+                    stdout=("output.txt", 'w')
                 )
             ])
         )
